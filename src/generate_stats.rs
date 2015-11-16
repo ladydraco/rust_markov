69fcef66b5191fd76f9cde5d9dd4746c800cf19a
Aaron Moore <two.a.ron@gmail.com>
2015-11-16 01:37:06 -0800

    Added collecting of sentence stats and basic attempts at inflation and suppression of sentence ending characters during output. Most of the way done with sentence stuff, I think.

--- src/generate_stats.rs
+++ src/generate_stats.rs
@@ -1,6 +1,37 @@
 
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::collections::VecDeque;
+
+#[derive(Debug)]
+pub struct Stats<'a> {
+	pub char_stats: Vec<OrderStats<'a>>,
+	pub sentence_stats: SentenceStats<'a>
+}
+
+impl<'a> Stats<'a> {
+	fn new(max_order: usize) -> Stats<'a> {
+		let mut char_stats: Vec<OrderStats> = Vec::new();
+		for _ in 0..max_order {
+			let order_stats = OrderStats {
+				total_usages: 0,
+				stats_for_state: HashMap::new()
+			};
+			char_stats.push(order_stats);
+		}
+
+		let sentence_stats = SentenceStats {
+			total_usages: 0,
+			starts: Vec::new(),
+			stats_for_state: HashMap::new()
+		};
+
+		Stats {
+			char_stats: char_stats,
+			sentence_stats: sentence_stats
+		}
+	}
+}
 
 #[derive(Debug)]
 pub struct OrderStats<'a> {
@@ -9,19 +40,19 @@ pub struct OrderStats<'a> {
 }
 
 impl<'a> OrderStats<'a> {
-	fn add_stats(& mut self, key: &'a str, next_char: char) {
+	fn add_stats(&mut self, state: &'a str, next: char) {
 		self.total_usages += 1;
 
-		if !self.stats_for_state.contains_key(key) {
+		if !self.stats_for_state.contains_key(state) {
 			let choice_stats = CharChoiceStats {
 				total_usages: 0,
 				options: HashMap::new()
 			};
-			self.stats_for_state.insert(key, choice_stats);
+			self.stats_for_state.insert(state, choice_stats);
 		}
 
-		let mut choice_stats = self.stats_for_state.get_mut(key).unwrap();
-		choice_stats.add_option(next_char);
+		let mut choice_stats = self.stats_for_state.get_mut(state).unwrap();
+		choice_stats.add_option(next);
 	}
 }
 
@@ -31,12 +62,51 @@ pub struct CharChoiceStats {
 	pub options: HashMap<char, i32>
 }
 
-pub trait AddOption<T> {
-	fn add_option(& mut self, T);
+impl CharChoiceStats {
+	fn add_option(&mut self, option: char) {
+		self.total_usages += 1;
+
+		if !self.options.contains_key(&option) {
+			self.options.insert(option, 0);
+		}
+
+		let mut char_count = self.options.get_mut(&option).unwrap();
+		*char_count += 1;
+	}
 }
 
-impl AddOption<char> for CharChoiceStats {
-	fn add_option(& mut self, option: char) {
+#[derive(Debug)]
+pub struct SentenceStats<'a> {
+	pub total_usages: i32,
+	pub starts: Vec<&'a str>,
+	pub stats_for_state: HashMap<i32, SentenceChoiceStats>
+}
+
+impl<'a> SentenceStats<'a> {
+	fn add_stats(&mut self, state: i32, next: i32) {
+		self.total_usages += 1;
+
+		if !self.stats_for_state.contains_key(&state) {
+			let choice_stats = SentenceChoiceStats {
+				total_usages: 0,
+				options: HashMap::new()
+			};
+			self.stats_for_state.insert(state, choice_stats);
+		}
+
+		let mut choice_stats = self.stats_for_state.get_mut(&state).unwrap();
+		choice_stats.add_option(next);
+	}
+}
+
+#[derive(Debug)]
+pub struct SentenceChoiceStats {
+	pub total_usages: i32,
+	pub options: HashMap<i32, i32>
+}
+
+impl SentenceChoiceStats {
+	fn add_option(&mut self, option: i32) {
 		self.total_usages += 1;
 
 		if !self.options.contains_key(&option) {
@@ -52,18 +122,79 @@ impl AddOption<char> for CharChoiceStats {
 //  a given character after a configurable (MAX_ORDER) number of
 //  characters has been encountered.
 
-pub fn gather_statistics(text: &str, max_order: usize) -> Vec<OrderStats> {
-	let mut stats: Vec<OrderStats> = Vec::new();
-	for _ in 0..max_order {
-		let order_stats = OrderStats {
-			total_usages: 0,
-			stats_for_state: HashMap::new()
-		};
-		stats.push(order_stats);
+pub struct SentenceWatcher {
+	pub word_count: i32,
+	pub enders: HashSet<char>,
+	
+	in_word: bool,
+	start: Option<usize>,
+	previous_length: Option<i32>,
+}
+
+impl SentenceWatcher {
+	pub fn new() -> SentenceWatcher {
+		let mut enders = HashSet::new();
+		enders.insert('.');
+		enders.insert('!');
+		enders.insert('?');
+
+		// 	let mut mid_word_punctuation = HashSet::new();
+		// 	mid_word_punctuation.insert('\'');
+		// 	mid_word_punctuation.insert('-');
+
+		SentenceWatcher {
+			word_count: 0,
+			in_word: false,
+			start: None,
+			previous_length: None,
+			enders: enders
+		}
 	}
+	fn watch_for_stats(&mut self, offset: usize, next_char: char) -> Option<(i32, usize, Option<i32>)> {
+		let mut sentence_ended = None;
+		if next_char.is_alphabetic() {
+			if !self.in_word {
+				self.in_word = true;
+			}
+			if self.start.is_none() {
+				self.start = Some(offset);
+			}
+		} else {
+			if self.in_word {
+				self.in_word = false;
+				self.word_count += 1;
+			}
+			if self.enders.contains(&next_char) {
+				if let Some(start) = self.start {
+					sentence_ended = Some((self.word_count, start, self.previous_length));
+					self.previous_length = Some(self.word_count);
+					self.word_count = 0;
+					self.start = None;
+				}
+			}
+			if next_char == '\n' {
+				self.start = None;
+			}
+		}
+		return sentence_ended;
+	}
+	pub fn watch(&mut self, next_char: char) -> Option<i32> {
+		if let Some((word_count, _, _)) = self.watch_for_stats(0, next_char) {
+			Some(word_count)
+		} else {
+			None
+		}
+	}
+}
+
+pub fn gather_statistics(text: &str, max_order: usize) -> Stats {
+
+	let mut stats = Stats::new(max_order);
 
 	// Iterate input text by character and extract statistics about each
 	//  order as we go.
+
+	let mut sentence_watcher = SentenceWatcher::new();
 
 	// A sliding window of length MAX_ORDER + 1 that captures the character offsets
 	//  of current character as well as the past MAX_ORDER characters. This allows
@@ -81,9 +212,7 @@ pub fn gather_statistics(text: &str, max_order: usize) -> Vec<OrderStats> {
 			window.pop_back();
 		}
 
-		// Look at the previous characters up to a max distance 
-		//  of MAX_ORDER. For each order, add statistics for this
-		//  following-character choice (next_char).
+		// Collect character-level stats for each order:
 
 		if window.len() > 1 {
 			for i in 1..window.len() {
@@ -96,10 +225,42 @@ pub fn gather_statistics(text: &str, max_order: usize) -> Vec<OrderStats> {
 				let end = window[0];
 				let key = &text[start..end];
 				
-				stats[ord].add_stats(key, next_char);
+				stats.char_stats[ord].add_stats(key, next_char);
+			}
+		}
+
+		// Collect sentence-length stats and mark valid sentence beginnings:
+
+		if let Some(sentence_ended) = sentence_watcher.watch_for_stats(offset, next_char) {
+			let (word_count, start, previous_length) = sentence_ended;
+
+			stats.sentence_stats.starts.push(&text[start..]);
+
+			if let Some(previous_length) = previous_length {
+				stats.sentence_stats.add_stats(previous_length, word_count);
 			}
 		}
 	}
+
+	// println!("{}", stats.sentence_stats.starts.len());
+	// let mut i = 0;
+	// for s in stats.sentence_stats.starts.iter() {
+	// 	println!("{}", &s[0..40]);
+	// 	println!("----");
+	// 	i += 1;
+	// 	if i == 20 {
+	// 		break;
+	// 	}
+	// }
+
+	// Print out sentence stats:
+	// for (key, val) in stats.sentence_stats.stats_for_state.iter() {
+	// 	println!("\"{}\":", key);
+
+	// 	for (key2, val2) in val.options.iter() {
+	// 		println!("   '{}' -> {}", key2, val2);
+	// 	}
+	// }
 
 	return stats;
 }
--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,14 @@ use std::process;
 use std::fs::File;
 use std::io::Write;
 use std::io::Read;
+use std::collections::HashMap;
 use rand::random;
 use num::traits::NumCast;
 use generate_stats::{
+	Stats,
 	OrderStats,
 	CharChoiceStats,
+	SentenceWatcher,
 	gather_statistics,
 	};
 
@@ -30,9 +33,8 @@ fn main() {
 	let text = load_book(&args.input_filename);
 	let stats = gather_statistics(&text, args.higher_order_bound);
 
-	generate_text(&stats, &args);
-
-	// debug_stats(&stats[args.higher_order_bound - 1]);
+	let mut generator = Generator::new(&args, &stats);
+	generator.generate_text(&args);
 
     println!("\nDone.");
 }
@@ -44,6 +46,7 @@ struct Args {
 	lower_order_bound: usize,
 	higher_order_bound: usize,
 	output_amount: usize,
+	use_html: bool
 }
 
 #[allow(dead_code)]
@@ -66,13 +69,15 @@ fn parse_arguments() -> Args {
 		output_filename: String::from(OUTPUT_FILE),
 		lower_order_bound: MIN_ORDER,
 		higher_order_bound: MAX_ORDER,
-		output_amount: OUTPUT_CHARS
+		output_amount: OUTPUT_CHARS,
+		use_html: false
 	};
 
 	for arg in env::args() {
 		match &arg[0..2] {
 			"-i" => parsed_args.input_filename = String::from(&arg[3..]),
 			"-o" => parsed_args.output_filename = String::from(&arg[3..]),
+			"-f" => parsed_args.use_html = true,
 			"-l" => parsed_args.lower_order_bound = parse_usize_or_default(&arg[3..], MIN_ORDER),
 			"-h" => parsed_args.higher_order_bound = parse_usize_or_default(&arg[3..], MAX_ORDER),
 			"-a" => parsed_args.output_amount = parse_usize_or_default(&arg[3..], OUTPUT_CHARS),
@@ -91,6 +96,7 @@ fn print_help() {
 	println!(" -l: low order bound (minimum order to use).");
 	println!(" -h: high order bound (maximum order to use).");
 	println!(" -a: amount of generated output in characters.");
+	println!(" -f: print out html with color coding indicating the order.");
 	println!(" -?: print help.");
 	process::exit(1);
 }
@@ -115,90 +121,219 @@ fn load_book(file_name: &str) -> String {
 	}
 }
 
-struct Generator<'a, 'b> {
-	output_file: &'a mut File,
-	stats: &'b Vec<OrderStats<'b>>,
+struct Generator<'a> {
+	output_file: File,
+	stats: &'a Stats<'a>,
 	current: String,
 	current_order: usize,
 	total: usize,
 	change_order_counter: i32,
+	current_sentence_length: i32,
+	distortions: CharChoiceStats,
+	sentence_watcher: SentenceWatcher,
+	use_html: bool,
 }
 
-fn generate_text(stats: &Vec<OrderStats>, args: &Args) {
-	let mut output_file = if let Ok(file) = File::create(&args.output_filename) {
-		file
-	} else {
-		panic!("Hey dumbass, there was a problem opening file.");
-	};
+impl<'a> Generator<'a> {
+	fn new(args: &Args, stats: &'a Stats<'a>) -> Generator<'a> {
+		let mut output_file = if let Ok(file) = File::create(&args.output_filename) {
+			file
+		} else {
+			panic!("Hey dumbass, there was a problem opening file.");
+		};
 
-	let mut generator = Generator {
-		output_file: &mut output_file,
-		stats: &stats,
-		current: String::new(),
-		current_order: args.higher_order_bound,
-		total: 0,
-		change_order_counter: 0,
-	};
+		if args.use_html {
+			let _ = output_file.write("<meta charset=\"UTF-8\">".as_bytes());
+			let _ = output_file.write("<style type=\"text/css\"> body { white-space: pre-wrap; } ".as_bytes());
+			for i in args.lower_order_bound..args.higher_order_bound + 1 {
+				
+				let a = args.higher_order_bound + 1 - args.lower_order_bound;
+				let b = i - args.lower_order_bound;
+				let c = a - b - 1;
+				let multiplier = c as f64 / a as f64;
+
+				let value = (multiplier * 248.0) as i32;
+				let _ = output_file.write(".order-".as_bytes());
+				let _ = output_file.write(i.to_string().as_bytes());
+				let _ = output_file.write("{ ".as_bytes());
+
+				let _ = output_file.write(" color: rgb(".as_bytes());
+				let _ = output_file.write(value.to_string().as_bytes());
+				let _ = output_file.write(",".as_bytes());
+				let _ = output_file.write(value.to_string().as_bytes());
+				let _ = output_file.write(",".as_bytes());
+				let _ = output_file.write(value.to_string().as_bytes());
+				let _ = output_file.write(");\n".as_bytes());
+
+				let _ = output_file.write("}\n".as_bytes());
+			}
+			let _ = output_file.write("</style>".as_bytes());
+		}
+
+		let mut generator = Generator {
+			output_file: output_file,
+			stats: stats,
+			current: String::new(),
+			current_order: args.higher_order_bound,
+			total: 0,
+			change_order_counter: 0,
+			current_sentence_length: 0,
+			distortions: CharChoiceStats {
+				total_usages: 0,
+				options: HashMap::new()
+			},
+			sentence_watcher: SentenceWatcher::new(),
+			use_html: args.use_html
+		};
+
+		generator.start();
+
+		return generator;
+	}
 
 	// Choose random starting string (encountered in the input text) 
 	//  of length MAX_ORDER.
 
-	let keys_count = generator.stats[args.higher_order_bound - 1].stats_for_state.len();
-	let choice_index = pick_random_in_range(0, keys_count - 1);
-	generator.current.push_str(*generator.stats[generator.current_order - 1].stats_for_state.keys().nth(choice_index).unwrap());
+	fn start(&mut self) {
+		let stats_for_state = &self.stats.char_stats[self.current_order - 1].stats_for_state;
+		let choice_index = pick_random_in_range(0, stats_for_state.len() - 1);
+		let start_string = stats_for_state.keys().nth(choice_index).unwrap();
+		self.current.push_str(*start_string);
 
-	let _ = generator.output_file.write(generator.current.as_bytes());
+		self.total += self.current.chars().count();
 
+		for next_char in self.current.chars() {
+			self.sentence_watcher.watch(next_char);
+		}
 
+		let start_output = self.current.clone();
+		self.output(&start_output);
+
+		let sentence_choice_index = pick_random_in_range(0, self.stats.sentence_stats.stats_for_state.len() - 1);
+		self.current_sentence_length = *self.stats.sentence_stats.stats_for_state.keys().nth(sentence_choice_index).unwrap();
+	}
 
 	// Generate characters that follow the starting string chosen by
 	//  following random paths through the generated statistics.
 
-	generator.total += generator.current.chars().count();
+	fn generate_text(&mut self, args: &Args) {
+		while self.total < args.output_amount {
+			let choice_stats = &self.stats.char_stats[self.current_order - 1].stats_for_state[&self.current[..]];
 
-	while generator.total < args.output_amount {
-		let choice_stats = &generator.stats[generator.current_order - 1].stats_for_state[&generator.current[..]];
+			self.update_order_used(&args);
+			self.calculate_distortions(&choice_stats);
+			self.generate_next_character(&choice_stats);
+		}
 
-		update_order_used(&args, &mut generator.change_order_counter, &mut generator.current_order);
-		generate_next_character(&choice_stats, &generator.current_order, &mut generator.current, &mut generator.output_file, &mut generator.total);
+		let _ = self.output_file.flush();
 	}
 
-	let _ = generator.output_file.flush();
-}
-
-fn update_order_used(args: &Args, change_order_counter: &mut i32, current_ord: &mut usize) {
-	if *change_order_counter == 0 {
-		if pick_random_in_range(0, 1) == 0 {
-			if *current_ord > args.lower_order_bound {
-				*current_ord -= 1;
+	fn update_order_used(&mut self, args: &Args) {
+		if self.change_order_counter == 0 {
+			if pick_random_in_range(0, 1) == 0 {
+				if self.current_order > args.lower_order_bound {
+					self.current_order -= 1;
+				}
+			} else {
+				if self.current_order < args.higher_order_bound {
+					self.current_order += 1;
+				}
 			}
-		} else {
-			if *current_ord < args.higher_order_bound {
-				*current_ord += 1;
+			self.change_order_counter = 0;
+		}
+		else {
+			self.change_order_counter += 1;
+		}
+	}
+
+	fn calculate_distortions(&mut self, choice_stats: &CharChoiceStats) {
+		self.distortions.total_usages = choice_stats.total_usages;
+		self.distortions.options.clear();
+		for (char_choice, count) in choice_stats.options.iter() {
+			if self.sentence_watcher.enders.contains(char_choice) {
+				let new_count = if self.current_sentence_length > self.sentence_watcher.word_count {
+					(*count as f64 * 0.01).ceil() as i32
+				} else {
+					count * 100
+				};
+				self.distortions.total_usages += new_count - count;
+				self.distortions.options.insert(*char_choice, new_count);
+			}
+			if *char_choice == '\n' {
+				let new_count = (*count as f64 * 0.01).ceil() as i32;
+				self.distortions.total_usages += new_count - count;
+				self.distortions.options.insert(*char_choice, new_count);
 			}
 		}
-		*change_order_counter = 0;
 	}
-	else {
-		*change_order_counter += 1;
-	}
-}
 
-fn generate_next_character(choice_stats: &CharChoiceStats, current_ord: &usize, current: &mut String, output_file: &mut File, total: &mut usize) {
-	let mut choice_num = pick_random_in_range(1, choice_stats.total_usages);
+	fn generate_next_character(&mut self, choice_stats: &CharChoiceStats) {
+		let mut choice_num = pick_random_in_range(1, self.distortions.total_usages);
 
-	for (next_char, count) in choice_stats.options.iter() {
-		choice_num = choice_num - count;
-
-		if choice_num <= 0 {
-			let _ = output_file.write(next_char.to_string().as_bytes());
-			current.push(*next_char);
-			*total += 1;
-			let remove_count = cmp::max(current.chars().count() - *current_ord, 0);
-			for _ in 0..remove_count {
-				current.remove(0);
+		for (next_char, next_count) in choice_stats.options.iter() {
+			let mut count = *next_count;
+			if let Some(count_override) = self.distortions.options.get(next_char) {
+				count = *count_override;
 			}
-			break;
+			choice_num -= count;
+
+			if choice_num <= 0 {
+				self.output(&next_char.to_string());
+
+				self.current.push(*next_char);
+				self.total += 1;
+
+				let remove_count = cmp::max(self.current.chars().count() - self.current_order, 0);
+				for _ in 0..remove_count {
+					self.current.remove(0);
+				}
+
+				if let Some(word_count) = self.sentence_watcher.watch(*next_char) {
+					if word_count < self.current_sentence_length {
+						println!("Too short by: {}", self.current_sentence_length - word_count);
+					} else if word_count > self.current_sentence_length {
+						println!("Too long by: {}", word_count - self.current_sentence_length);
+					} else {
+						println!("Correct sentence length! Huzzah!");
+					}
+					self.generate_next_sentence_length();
+				}
+
+				break;
+			}
+		}
+	}
+
+	fn output(&mut self, string: &String) {
+		if self.use_html {
+			let _ = self.output_file.write("<span class=\"order-".as_bytes());
+			let _ = self.output_file.write(self.current.chars().count().to_string().as_bytes());
+			let _ = self.output_file.write("\">".as_bytes());
+		}
+
+		let _ = self.output_file.write(string.as_bytes());
+
+		if self.use_html {
+			let _ = self.output_file.write("</span>".as_bytes());
+		}
+	}
+
+	fn generate_next_sentence_length(&mut self) {
+		let stats_for_state = &self.stats.sentence_stats.stats_for_state;
+		if let Some(choice_stats) = stats_for_state.get(&self.current_sentence_length) {
+			let mut choice_num = pick_random_in_range(1, choice_stats.total_usages);
+
+			for (next_length, count) in choice_stats.options.iter() {
+				choice_num = choice_num - count;
+
+				if choice_num <= 0 {
+					self.current_sentence_length = *next_length;
+					break;
+				}
+			}
+		} else {
+			let sentence_choice_index = pick_random_in_range(0, stats_for_state.len() - 1);
+			self.current_sentence_length = *stats_for_state.keys().nth(sentence_choice_index).unwrap();
 		}
 	}
 }
